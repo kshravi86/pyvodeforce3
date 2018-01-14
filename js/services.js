47aadba343ea0ef7543ba5d938928fbdde2c5e8e
kshravi86 <kshravi86@gmail.com>
2018-01-14 13:57:45 +0530

    bdbdh

--- js/services.js
+++ js/services.js
@@ -1,180 +1,50 @@
-'use strict';
+angular.module('starter.services', [])
 
-/* Services */
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
 
-// Simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  },{
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
 
-// phonegap ready service - listens to deviceready
-myApp.factory('phonegapReady', function() {
-    return function (fn) {
-        var queue = [];
-        var impl = function () {
-        queue.push(Array.prototype.slice.call(arguments));
-    };
-              
-    document.addEventListener('deviceready', function () {
-        queue.forEach(function (args) {
-            fn.apply(this, args);
-        });
-        impl = fn;
-    }, false);
-              
-    return function () {
-        return impl.apply(this, arguments);
-        };
-    };
-});
-
-myApp.factory('geolocation', function ($rootScope, phonegapReady) {
   return {
-    getCurrentPosition: function (onSuccess, onError, options) {
-        navigator.geolocation.getCurrentPosition(function () {
-               var that = this,
-               args = arguments;
-
-               if (onSuccess) {
-                   $rootScope.$apply(function () {
-                        onSuccess.apply(that, args);
-                   });
-                   }
-               }, function () {
-                    var that = this,
-                    args = arguments;
-
-                   if (onError) {
-                        $rootScope.$apply(function () {
-                            onError.apply(that, args);
-                        });
-                   }
-               },
-            options);
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
         }
-    };
-});
-
-myApp.factory('accelerometer', function ($rootScope, phonegapReady) {
-    return {
-        getCurrentAcceleration: phonegapReady(function (onSuccess, onError) {
-            navigator.accelerometer.getCurrentAcceleration(function () {
-                var that = this,
-                    args = arguments;
-
-                if (onSuccess) {
-                    $rootScope.$apply(function () {
-                        onSuccess.apply(that, args);
-                    });
-                }
-            }, function () {
-                var that = this,
-                args = arguments;
-
-                if (onError) {
-                    $rootScope.$apply(function () {
-                        onError.apply(that, args);
-                    });
-                }
-            });
-        })
-    };
-});
-
-myApp.factory('notification', function ($rootScope, phonegapReady) {
-    return {
-        alert: phonegapReady(function (message, alertCallback, title, buttonName) {
-            navigator.notification.alert(message, function () {
-                var that = this,
-                    args = arguments;
-
-                $rootScope.$apply(function () {
-                    alertCallback.apply(that, args);
-                });
-            }, title, buttonName);
-        }),
-        confirm: phonegapReady(function (message, confirmCallback, title, buttonLabels) {
-            navigator.notification.confirm(message, function () {
-                var that = this,
-                    args = arguments;
-
-                $rootScope.$apply(function () {
-                    confirmCallback.apply(that, args);
-                });
-            }, title, buttonLabels);
-        }),
-        beep: function (times) {
-            navigator.notification.beep(times);
-        },
-        vibrate: function (milliseconds) {
-            navigator.notification.vibrate(milliseconds);
-        }
-    };
-});
-
-myApp.factory('navSvc', function($navigate) {
-    return {
-        slidePage: function (path,type) {
-            $navigate.go(path,type);
-        },
-        back: function () {
-            $navigate.back();
-        }
+      }
+      return null;
     }
+  };
 });
-
-myApp.factory('compass', function ($rootScope, phonegapReady) {
-    return {
-        getCurrentHeading: phonegapReady(function (onSuccess, onError) {
-            navigator.compass.getCurrentHeading(function () {
-                var that = this,
-                    args = arguments;
-
-                if (onSuccess) {
-                    $rootScope.$apply(function () {
-                        onSuccess.apply(that, args);
-                    });
-                }
-            }, function () {
-                var that = this,
-                    args = arguments;
-
-                if (onError) {
-                    $rootScope.$apply(function () {
-                        onError.apply(that, args);
-                    });
-                }
-            });
-        })
-    };
-});
-
-myApp.factory('contacts', function ($rootScope, phonegapReady) {
-    return {
-        findContacts: phonegapReady(function (onSuccess, onError) {
-            var options = new ContactFindOptions();
-            options.filter="";
-            options.multiple=true;
-            var fields = ["displayName", "name"];
-            navigator.contacts.find(fields, function(r){console.log("Success" +r.length);var that = this,
-                args = arguments;
-                if (onSuccess) {
-                    $rootScope.$apply(function () {
-                        onSuccess.apply(that, args);
-                    });
-                }
-            }, function () {
-                var that = this,
-                    args = arguments;
-
-                if (onError) {
-                    $rootScope.$apply(function () {
-                        onError.apply(that, args);
-                    });
-                }
-            }, options)
-        })
-    }
-});
-
-
-
