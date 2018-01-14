7e5a98641d2c7886b4066af203e756da1190f141
kshravi86 <kshravi86@gmail.com>
2018-01-14 14:09:11 +0530

    vvg

--- js/admob.js
+++ js/admob.js
@@ -1,8 +1,8 @@
 var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-6869992474017983/9375997553',
-        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+        banner: 'ca-app-pub-5830283354936773/1537970274',
+        interstitial: 'ca-app-pub-5830283354936773/6256724737'
     };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
