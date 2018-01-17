f5374453dcd990ccb1adc9b8c9bc9dbf071a06f8
kshravi86 <kshravi86@gmail.com>
2018-01-17 22:15:37 +0530

    ghh

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
         $routeProvider.when('/', {templateUrl: 'partials/homeView.html', controller: 'HomeCtrl'});
         $routeProvider.when('/view1', {templateUrl: 'partials/Untitled1.html'});
         $routeProvider.when('/view2', {templateUrl: 'partials/Untitled2.html'});
-        $routeProvider.when('/view3', {templateUrl: 'partials/helloworld.html'});
+        $routeProvider.when('/view3', {templateUrl: 'partials/Untitled3.html'});
         $routeProvider.when('/view4', {templateUrl: 'partials/basic_operations.html'});
         $routeProvider.when('/view5', {templateUrl: 'partials/logistic_regression.html'});
         $routeProvider.when('/view6', {templateUrl: 'partials/BasicTensorFlow.html'});
