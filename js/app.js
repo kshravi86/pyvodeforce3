1502e3e7b17e9289e98b8aab6c9503fbdf57b9c8
kshravi86 <kshravi86@gmail.com>
2018-02-10 17:31:46 +0530

    hhh

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
         $routeProvider.when('/view5', {templateUrl: 'partials/Untitled5.html'});
         $routeProvider.when('/view6', {templateUrl: 'partials/Untitled6.html'});
         $routeProvider.when('/view7', {templateUrl: 'partials/Untitled7.html'});
-        $routeProvider.when('/view8', {templateUrl: 'partials/for.html'});
+        $routeProvider.when('/view8', {templateUrl: 'partials/Pandas.html'});
 		
        $routeProvider.when('/view10',{templateUrl:'partials/Untitled7.html'});
 	   $routeProvider.when("/view11",{templateUrl:'partials/Untitled11.html'});
